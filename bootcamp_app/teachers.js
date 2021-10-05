215c21148ae34fe1befabef63007801935f08278
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-10-05 14:25:40 +0000

    creates student and teacher queries using node-postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,36 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.connect()
+  .then((client) => {
+    console.log('connected');
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+
+const myArgs = process.argv.slice(2);
+
+pool.query(`
+SELECT DISTINCT cohorts.name as cohort, teachers.name as teacher
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher
+  `)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
+
